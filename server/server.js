051c81f04cf8aeebfd95b9ab09cea002e3cee845
ladka6 <ege-erdal101@hotmail.com>
2022-09-05 12:05:16 +0300

    askım

--- server/server.js
+++ server/server.js
@@ -1,17 +1,7 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 app.use(express.json());
 
-const users=[]
 
-app.get('users',(req,res)=> {
-    res.json(users)
-})
-
-app.get('users',(req,res)=> {
-   const user={name: res.body.name,password: req.body.password}
-   users.push(user)
-   res.status(201).send()
-})
 
 app.listen(3000)
