8ef04baa594378777c39e3e2e71c9310aff16739
efsakts <efsaaktas@outlook.com>
2022-09-05 12:09:37 +0300

    askim

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+const bcrypt=require('bcrypt');
 app.use(express.json());
 
 const users=[]
@@ -8,10 +9,20 @@ app.get('users',(req,res)=> {
     res.json(users)
 })
 
-app.get('users',(req,res)=> {
-   const user={name: res.body.name,password: req.body.password}
+app.post('users', async (req,res)=> {
+    try{
+        const salt=await bcrypt.genSalt();
+        const hashedPassword=await bcrypt.hash(req.body.password,salt)
+        console.log(salt)
+        console.log(hashedPassword)
+    
+   const user={name: res.body.name,password: hashedPassword}
    users.push(user)
    res.status(201).send()
+   bcrypt.hash(salt + 'password')
+ } catch{
+    res.status(500).send()  
+}
 })
 
 app.listen(3000)
